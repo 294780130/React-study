7525c76eaa91e37695e068dbf3b5006cf9f45ab0
294780130 <294780130@qq.com>
2018-05-10 13:40:45 +0800

    day05

---
+++ day05/js/source/app.js
@@ -0,0 +1,21 @@
+/*
+* @Author: Administrator
+* @Date:   2018-05-09 17:53:21
+* @Last Modified by:   Administrator
+* @Last Modified time: 2018-05-09 18:08:09
+*/
+
+
+
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Logo from './components/Logo';
+
+React.render(
+	<h1>
+		<Logo />Welcome to The App!
+	</h1>,
+	document.getElementById('app')
+);
---
+++ day05/js/source/components/Logo.js
@@ -0,0 +1,15 @@
+/*
+* @Author: Administrator
+* @Date:   2018-05-09 17:53:42
+* @Last Modified by:   Administrator
+* @Last Modified time: 2018-05-09 18:21:07
+*/
+import React from 'react';
+
+class Logo extends React.Component{
+	render(){
+		return <div className="Logo" />;
+	}
+}
+
+export default Logo
